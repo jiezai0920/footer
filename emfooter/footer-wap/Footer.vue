172799075b283f223ff8a82ded77887523a66b22
jiezai0920 <351510576@qq.com>
2019-09-11 16:44:49 +0800

    fix($emfooter): fix emlogin version

--- emfooter/footer-wap/Footer.vue
+++ emfooter/footer-wap/Footer.vue
@@ -18,8 +18,9 @@
     }" href="javascript:;" v-if="complaint" @click="goComplaint">投诉</a>
     <modal-wap v-show="confirmStatus" @cancel="cancelExit" @ok="goExit" :style="{ zIndex: zIndexModal }" v-if="isAll" :title="modalTitle">{{modalContent}}</modal-wap>
     <toast-wap ref="toast" v-if="isAll"></toast-wap>
-    <w-login
+    <w-login-wap
       v-if="isAll"
+      style="z-index:101;"
       :domain="newDomain"
       :show="loginStatus"
       :close="loginCloseFn"
@@ -29,7 +30,21 @@
       :sendAction="sendAction"
       :loginAction="loginAction"
       :style="{zIndex: zIndexLogin}"
-    ></w-login>
+      :lang="lang"
+      :sendEnglishAction="sendEnglishAction"
+      :loginEnglishAction="loginEnglishAction"
+      :title="loginTitle"
+      :btnText="btnText"
+      :subingText="subingText"
+      :sendingText="sendingText"
+      :telEmptyText="telEmptyText"
+      :telFormatErrorText="telFormatErrorText"
+      :smsBtnText="smsBtnText"
+      :smsPlaceholder="smsPlaceholder"
+      :telPlaceholder="telPlaceholder"
+      :desc="desc"
+    ></w-login-wap>
+
   </div>
 </template>
 <script>
@@ -118,6 +133,52 @@ export default {
     countrycodeAction: String,
     sendAction: String,
     loginAction: String,
+    lang: {
+      type: String,
+      default: 'zh_CN',
+    },
+    sendEnglishAction: String,
+    loginEnglishAction: String,
+    loginTitle: {
+      type: String,
+      default: '请完善手机信息',
+    },
+    desc: {
+      type: String,
+      default: '成功短信将发送至该手机',
+    },
+    telPlaceholder: {
+      type: String,
+      default: '输入手机号',
+    },
+    smsPlaceholder: {
+      type: String,
+      default: '输入验证码',
+    },
+    smsBtnText: {
+      type: String,
+      default: '获取验证码',
+    },
+    telFormatErrorText: {
+      type: String,
+      default: '手机号格式有误',
+    },
+    telEmptyText: {
+      type: String,
+      default: '输入手机号',
+    },
+    sendingText: {
+      type: String,
+      default: '获取中',
+    },
+    subingText: {
+      type: String,
+      default: '正在提交...',
+    },
+    btnText: {
+      type: String,
+      default: '确定',
+    },
     // 登录相关 end
   },
   computed: {
