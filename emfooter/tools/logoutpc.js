595b2ad3f3a9cb7378dda15feb80eeb7aedcd4ee
jiezai <351510576@qq.com>
2020-07-21 19:56:35 +0800

    fix($footer): fix login version

--- emfooter/tools/logoutpc.js
+++ emfooter/tools/logoutpc.js
@@ -1,40 +1,19 @@
-/* eslint-disable */
-import 'em-jsonp';
+export default (orgId, self, callback) => {
+  const domainStr = self.domain || '';
 
-const afterLogin = (callbackUrl, inow, self, callback) => {
-  let theIndex = inow;
-  const urlSplit = callbackUrl[theIndex].split('?');
-  const newUrl = urlSplit[0];
-  let data = urlSplit[1];
-  data = data.replace(/=/g, '":"').replace(/&/g, '","');
-  data = JSON.parse(`{"${data}"}`);
-  window.$jsonp({
-    url: newUrl,
-    data,
-    success: (res) => {
-      if (res.code === 10000) {
-        if (theIndex === 0) {
-          callback();
-        } else {
-          afterLogin(callbackUrl, --theIndex, self, callback);
+  setTimeout(() => {
+    window.$cookie.remove(`EMTOKEN_${orgId}`, '/', domainStr);
+    window.$cookie.remove('EMTOKEN', '/', domainStr);
+
+    /* eslint-disable */
+    const keys = document.cookie.match(/[^ =;]+(?=\=)/g);
+    if(keys) {
+      for (var i = keys.length; i--;) {
+        if (keys[i].indexOf('EMTOKEN') > -1) {
+          window.$cookie.remove(keys[i], '/', domainStr);
         }
-      } else {
-        self.handleAjaxError();
       }
-    },
-    error: () => {
-      self.handleAjaxError();
-    },
-  });
-};
-
-export default (result, orgId, self, callback) => {
-  afterLogin(result.data.url_list, result.data.url_list.length - 1, self, () => {
-    let domainStr = self.newDomain || '';
-
-    setTimeout(() => {
-      window.$cookie.remove(`Authorization?org_id=${orgId}`, '/', domainStr);
-      callback();
-    }, 100);
-  });
+    }
+    callback();
+  }, 100);
 };
